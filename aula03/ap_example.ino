9082d8dba1e838b9dcf1c079882085aa42a44375
Leonardo Dias do Carmo <leonardo_carmo@usp.br>
2026-05-26 22:57:39 -0300

    feat: updated code for control GPIOs via HTTP request, parse and operate in C

--- aula03/ap_example.ino
+++ aula03/ap_example.ino
@@ -1,45 +1,141 @@
-/*
+/*********
   Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+  Complete project details at https://RandomNerdTutorials.com/esp32-async-web-server-espasyncwebserver-library/
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
 
 // Import required libraries
-#include "WiFi.h"
-#include "ESPAsyncWebServer.h"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-// Set your access point network credentials
-const char* ssid = "labprocmaneiro";
-const char* password = "11235813";
+// Replace with your network credentials
+const char* ssid = "iPhone";
+const char* password = "07092005";
+
+const long int PIN_LED1 = 23;
+const long int PIN_LED2 = 22;
+const long int PIN_LED3 = 21;
+const long int PIN_LED4 = 19;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
+int8_t parse_to_int8(String a) ->  {
+  long int value = strtol(a.c_str(), NULL, 2);
+  int8_t result = (int8_t)value;
+}
+
+void setGPIOs(int8_t value) {
+  digitalWrite(PIN_LED1, value & 0x01);
+  digitalWrite(PIN_LED2, (value >> 1) & 0x01);
+  digitalWrite(PIN_LED3, (value >> 2) & 0x01);
+  digitalWrite(PIN_LED4, (value >> 3) & 0x01);
+}
+
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
-  Serial.println();
-  
-  // Setting the ESP as an access point
-  Serial.print("Setting AP (Access Point)…");
 
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
+  // Seta os pinos dos LEDs para output
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_LED3, OUTPUT);
+  pinMode(PIN_LED4, OUTPUT);
 
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
 
-  server.on("/somar", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", "Hello, World!");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html);
   });
-  
-  bool status;
 
+  // Send a GET request to <ESP_IP>/calc?a=<valueA>&b=<valueB>&op=<operation>
+  server.on("/calc", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String a, b, op;
+    // GET input1 value on <ESP_IP>/calc?a=<valueA>&b=<valueB>&op=<operation>
+    if (request->hasParam("a") && request->hasParam("b") && request->hasParam("op")) {
+      a = request->getParam("a")->value();
+      b = request->getParam("b")->value();
+      op = request->getParam("op")->value();
+    }
+    int8_t valueA = parse_to_int8(a);
+    int8_t valueB = parse_to_int8(b);
+
+    if (op == "sum") {
+      int8_t result = (valueA + valueB) & 0x0F; 
+      request->send(200, "text/plain", String(result));
+    } else if (op == "sub") {
+      int8_t result = (valueA - valueB) & 0x0F; 
+      request->send(200, "text/plain", String(result));
+    } else {
+      request->send(400, "text/plain", "Invalid operation");
+    }
+    
+    setGPIOs(result);
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Start server
   server.begin();
 }
- 
-void loop(){
-  
+
+void loop() {
+
 }
+
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP32 Calculator</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    body {max-width: 600px; margin:0px auto; padding: 25px;}
+    input {width: 100px; font-size: 1.2rem; margin: 8px;}
+    button {font-size: 1.1rem; padding: 10px 14px; margin: 8px;}
+    #result {font-size: 1.4rem; margin-top: 20px; display: block;}
+  </style>
+</head>
+<body>
+  <h2>ESP32 Calculator</h2>
+  <input type="number" id="valueA" placeholder="Value A">
+  <input type="number" id="valueB" placeholder="Value B">
+  <div>
+    <button onclick="sendCalc('sum')">Sum</button>
+    <button onclick="sendCalc('sub')">Subtract</button>
+  </div>
+  <span id="result">Result: </span>
+<script>
+function sendCalc(op) {
+  var a = document.getElementById('valueA').value;
+  var b = document.getElementById('valueB').value;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        document.getElementById('result').innerText = 'Result: ' + xhr.responseText;
+      } else {
+        document.getElementById('result').innerText = 'Error';
+      }
+    }
+  };
+  xhr.open('GET', '/calc?a=' + encodeURIComponent(a) + '&b=' + encodeURIComponent(b) + '&op=' + encodeURIComponent(op), true);
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
--- aula03/ap_tutorial.ino
+++ aula03/ap_tutorial.ino
@@ -1,86 +1,131 @@
-/*
+/*********
   Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+  Complete project details at https://RandomNerdTutorials.com/esp32-async-web-server-espasyncwebserver-library/
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
 
 // Import required libraries
-#include "WiFi.h"
-#include "ESPAsyncWebServer.h"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-// Set your access point network credentials
-const char* ssid = "ESP32-Access-Point";
-const char* password = "123456789";
-
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+const char* PARAM_INPUT_1 = "output";
+const char* PARAM_INPUT_2 = "state";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-String readTemp() {
-  return String(bme.readTemperature());
-  //return String(1.8 * bme.readTemperature() + 32);
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+    input:checked+.slider {background-color: #b30000}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons = "";
+    buttons += "<h4>Output - GPIO 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(2) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Output - GPIO 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"4\" " + outputState(4) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Output - GPIO 33</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"33\" " + outputState(33) + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
 }
 
-String readHumi() {
-  return String(bme.readHumidity());
-}
-
-String readPres() {
-  return String(bme.readPressure() / 100.0F);
+String outputState(int output){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
 }
 
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
-  Serial.println();
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  pinMode(33, OUTPUT);
+  digitalWrite(33, LOW);
   
-  // Setting the ESP as an access point
-  Serial.print("Setting AP (Access Point)…");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
-
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", readTemp().c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", readHumi().c_str());
-  });
-  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", readPres().c_str());
-  });
-  
-  bool status;
-
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
-  
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage1;
+    String inputMessage2;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    }
+    else {
+      inputMessage1 = "No message sent";
+      inputMessage2 = "No message sent";
+    }
+    Serial.print("GPIO: ");
+    Serial.print(inputMessage1);
+    Serial.print(" - Set to: ");
+    Serial.println(inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+
   // Start server
   server.begin();
 }
- 
-void loop(){
-  
+
+void loop() {
+
 }
