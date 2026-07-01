330e85d170138a91feb15e5e2f6cc803d3853b15
Leonardo Dias do Carmo <leonardo_carmo@usp.br>
2026-07-01 14:25:08 -0300

    add overflow e comp 2

--- aula07/calc4bits.c
+++ aula07/calc4bits.c
@@ -3,47 +3,94 @@
 
 #define MASK_4BIT 0x0F
 
+int32_t estender_sinal_4bits(int32_t val) {
+    val = val & MASK_4BIT;
+    if (val & 0x08) {
+        return (int32_t)(val | ~MASK_4BIT);
+    }
+    return val;
+}
+
 int main() {
     int num1, num2;
     char operador;
-    int resultado;
+    int resultado_puro;
+    int resultado_sinalizado;
+    int overflow = 0;
     
-    printf("Digite o primeiro inteiro (4 bits): ");
-    scanf("%d", &num1);
-    
-    printf("Digite o segundo inteiro (4 bits): ");
-    scanf("%d", &num2);
+    printf("Digite o primeiro inteiro (-8 a 7): ");
+    if (scanf("%d", &num1) != 1) return 1;
     
     printf("Digite o operador (+, -, *, !): ");
-    scanf(" %c", &operador);
+    if (scanf(" %c", &operador) != 1) return 1;
     
-    num1 = num1 & MASK_4BIT;
-    num2 = num2 & MASK_4BIT;
+    if (operador != '!') {
+        printf("Digite o segundo inteiro (-8 a 7): ");
+        if (scanf("%d", &num2) != 1) return 1;
+    }
+    
+    int n1 = estender_sinal_4bits(num1);
+    int n2 = estender_sinal_4bits(num2);
     
     switch(operador) {
         case '+':
-            resultado = (num1 + num2) & MASK_4BIT;
-            printf("Resultado: %d\n", resultado);
+            resultado_sinalizado = n1 + n2;
+            
+            if ((n1 > 0 && n2 > 0 && resultado_sinalizado <= 0) || 
+                (n1 < 0 && n2 < 0 && resultado_sinalizado >= 0)) {
+                overflow = 1;
+            }
             break;
+            
         case '-':
-            resultado = (num1 - num2) & MASK_4BIT;
-            printf("Resultado: %d\n", resultado);
+            resultado_sinalizado = n1 - n2;
+            
+            if ((n1 > 0 && n2 < 0 && resultado_sinalizado <= 0) || 
+                (n1 < 0 && n2 > 0 && resultado_sinalizado >= 0)) {
+                overflow = 1;
+            }
             break;
+            
         case '*':
-            resultado = (num1 * num2) & MASK_4BIT;
-            printf("Resultado: %d\n", resultado);
+            resultado_sinalizado = n1 * n2;
+            
+            if (resultado_sinalizado < -8 || resultado_sinalizado > 7) {
+                overflow = 1;
+            }
             break;
+            
         case '!': {
-            unsigned long long fatorial = 1;
-            for(int i = 1; i <= num1; i++) {
+            if (n1 < 0) {
+                printf("Erro: Fatorial de número negativo não existe!\n");
+                return 1;
+            }
+            
+            long long fatorial = 1;
+            for(int i = 1; i <= n1; i++) {
                 fatorial *= i;
             }
-            resultado = fatorial & MASK_4BIT;
-            printf("Resultado: %d\n", resultado);
+            
+            resultado_sinalizado = (int)fatorial;
+            if (resultado_sinalizado < -8 || resultado_sinalizado > 7) {
+                overflow = 1;
+            }
             break;
         }
         default:
             printf("Operador inválido!\n");
+            return 1;
+    }
+    
+    resultado_puro = resultado_sinalizado & MASK_4BIT;
+    
+    int exibicao_final = estender_sinal_4bits(resultado_puro);
+    
+    printf("\n--- RESULTADO EM 4 BITS ---\n");
+    printf("Resultado (Sinalizado): %d\n", exibicao_final);
+    printf("Resultado (Bits puros) : 0x%X\n", resultado_puro);
+    
+    if (overflow) {
+        printf("AVISO: Ocorreu OVERFLOW! O resultado real estourou o limite de 4 bits (-8 a 7).\n");
     }
     
     return 0;
