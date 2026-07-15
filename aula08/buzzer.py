185da923bc0931dfbdcbb1db1de8909a61c82378
Leonardo Dias do Carmo <leonardo_carmo@usp.br>
2026-07-15 13:38:34 -0300

    fix buzzer

--- aula08/buzzer.py
+++ aula08/buzzer.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python3
 ########################################################################
-# Filename    : Alertor.py
-# Description : Make Alertor with buzzer and button
+# Filename    : Doorbell.py
+# Description : Make doorbell with buzzer and button
 # Author      : www.freenove.com
 # modification: 2024/07/29
 ########################################################################
-from gpiozero import TonalBuzzer,Button
-from gpiozero.tones import Tone
+from gpiozero import Buzzer, Button  
 import time
-import math
 
-buzzer = TonalBuzzer(4)
-button = Button(20) # define Button pin according to BCM Numbering
+buzzer = Buzzer(12)
+button = Button(21)
+
+def onButtonPressed():
+    buzzer.on()
+    print("Button is pressed, buzzer turned on >>>")
+    
+def onButtonReleased():
+    buzzer.off()
+    print("Button is released, buzzer turned on <<<")
 
 def loop():
-    while True:
-        if button.is_pressed:  # if button is pressed
-            alertor()
-            print ('alertor turned on >>> ')
-        else :
-            stopAlertor()
-            print ('alertor turned off <<<')
-            
-def alertor():
-    buzzer.play(Tone(220.0)) 
-    time.sleep(0.1)
+    button.when_pressed = onButtonPressed
+    button.when_released = onButtonReleased
+    while True :
+        time.sleep(1)
         
-def stopAlertor():
-    buzzer.stop()
-            
 def destroy():
-    buzzer.close() 
-    button.close()     
+    buzzer.close()
+    button.close()
 
 if __name__ == '__main__':     # Program entrance
-    print ('Program is starting...')
+    print ('Program is starting ... ')
     try:
         loop()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
